354a1a9baa7ec836fbb0a925e376698f674c8179
AlexeySkillman <Alexey.skillman@gmail.com>
2022-02-05 22:07:32 +0500

    First Commit

---
+++ app/src/main/java/com/example/portfoliocard/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.example.portfoliocard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.portfoliocard.ui.theme.PortfolioCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PortfolioCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    CreateBizCard()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun CreateBizCard(){
+
+    // Surface Main Bg for us
+    Surface( // Surface переписывает ребенка
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+        ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp) // Зaчем если Surface Указывает По высоте оно не стало выше экрана и меньше тоже из Surface
+                .height(390.dp) // Зaчем если Surface Указывает По высоте оно не стало выше экрана и меньше тоже из Surface
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = 4.dp) {
+
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally ) {
+                CreateImageProfile()
+                Divider( thickness = 1.dp )
+                CreateInfo()
+            }
+
+        }
+
+
+    }
+}
+
+@Composable
+private fun CreateInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+
+        Text(
+            text = "Alexey O.",
+            style = MaterialTheme.typography.h4,
+            color = MaterialTheme.colors.primaryVariant
+        )
+        Text(
+            text = "Android Compose Programmer",
+            modifier = Modifier.padding(3.dp)
+        )
+        Text(
+            text = "@myPortfolioLink",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.subtitle1
+        )
+
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "profile image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+
+    }
+}
+
+// @Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PortfolioCardTheme {
+        CreateBizCard()
+    }
+}
