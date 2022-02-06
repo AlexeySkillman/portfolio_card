506527bc90a383176260050d4d1736b6e4bc72c1
AlexeySkillman <Alexey.skillman@gmail.com>
2022-02-06 11:10:35 +0500

    Finish

--- app/src/main/java/com/example/portfoliocard/MainActivity.kt
+++ app/src/main/java/com/example/portfoliocard/MainActivity.kt
@@ -7,16 +7,22 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.portfoliocard.ui.theme.PortfolioCardTheme
@@ -36,7 +42,9 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun CreateBizCard(){
-
+    var buttonClickedState = remember{
+        mutableStateOf(false)
+    }
     // Surface Main Bg for us
     Surface( // Surface переписывает ребенка
         modifier = Modifier
@@ -58,11 +66,77 @@ fun CreateBizCard(){
                 CreateImageProfile()
                 Divider( thickness = 1.dp )
                 CreateInfo()
+                Button(onClick = {
+                    buttonClickedState.value = !buttonClickedState.value
+                }) {
+                    Text(text = "Portfolio",
+                        style = MaterialTheme.typography.button  )
+                }
+                if(buttonClickedState.value){
+                    Content()
+                } else {
+                    Box() {}
+                }
             }
 
         }
 
 
+    }
+}
+
+@Composable
+fun Content(){
+    Box(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .padding(5.dp)){
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+            ) {
+
+            Portfolio(data = listOf(
+                                "Project 1",
+                                "Project 2",
+                                "Project 3",
+                                "Project 4",
+                                "Project 5"))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn{
+        // items импортируется import androidx.compose.foundation.lazy.items
+        items(data){ item ->
+            Card(modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth(),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Row(modifier = Modifier
+                    .padding(15.dp)
+                    .background(MaterialTheme.colors.surface)
+                ) {
+                    CreateImageProfile(modifier = Modifier.size(80.dp))
+                    Column( modifier = Modifier
+                        .padding(7.dp)
+                        .align(alignment = Alignment.CenterVertically)
+                    ) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "A great Project", style = MaterialTheme.typography.body2)
+                    }
+                }
+
+            }
+        }
     }
 }
 
@@ -91,7 +165,7 @@ private fun CreateInfo() {
 @Composable
 private fun CreateImageProfile(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
@@ -103,7 +177,7 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
         Image(
             painter = painterResource(id = R.drawable.profile_image),
             contentDescription = "profile image",
-            modifier = Modifier.size(135.dp),
+            modifier = modifier.size(135.dp),
             contentScale = ContentScale.Crop
         )
 
